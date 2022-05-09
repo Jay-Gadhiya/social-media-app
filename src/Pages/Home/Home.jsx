c60cf5615ea1c92546db56dabfe49e288ad407ba
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-05-09 23:27:07 +0530

    refactor

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -32,7 +32,7 @@ export const HomePage = () => {
                             <User imgUrl = {"https://picsum.photos/seed/picsum/200/300"}/>
                         </div>
                     </div>
-                    <div className="w-full md:hidden flex justify-between fixed bottom-0 p-3 bg-white bg-opacity-90">
+                    <div className="w-full md:hidden flex justify-between fixed bottom-0 p-3 bg-white">
                         <ImHome className='text-2xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
                         <MdAddBox className='text-3xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
                         <MdExplore className='text-3xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
