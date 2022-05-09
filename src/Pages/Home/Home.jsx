43a3b1fdac3d2d8816dfcc2ddfd26b08cd002d7e
Jay-Gadhiya <jaygadhiya9988@gmail.com>
2022-05-09 23:19:04 +0530

    refactor css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -32,11 +32,11 @@ export const HomePage = () => {
                             <User imgUrl = {"https://picsum.photos/seed/picsum/200/300"}/>
                         </div>
                     </div>
-                    <div className="w-full md:hidden flex justify-between fixed bottom-0 p-2 pt-3 bg-white bg-opacity-9w0">
-                        <ImHome className='text-xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
-                        <MdAddBox className='text-2xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
-                        <MdExplore className='text-2xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
-                        <IoNotificationsSharp className='text-2xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
+                    <div className="w-full md:hidden flex justify-between fixed bottom-0 p-3 bg-white bg-opacity-9w0">
+                        <ImHome className='text-2xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
+                        <MdAddBox className='text-3xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
+                        <MdExplore className='text-3xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
+                        <IoNotificationsSharp className='text-3xl cursor-pointer hover:text-blue-500 text-zinc-500'/>
                     </div>
             </main>
         </>
